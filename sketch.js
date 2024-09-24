302534f0011a9a390d2633182b10dce5820b2ce7
anacllarandrade <00001109932170sp@al.educacao.sp.gov.br>
2024-09-24 17:48:35 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,40 @@
+// a viagem de chihiro
+// guardiões da galáxia
+// as aventuras de pi
+// paddington
+// ladrões de bicicleta
+
+let campoIdade;
+let campoFantasia;
+
+function setup() {
+    createCanvas(400, 400);
+    campoIdade = createInput("15");
+    campoFantasia = createCheckbox("Gosta de Fantasia ?");
+}
+function draw() {
+    background(220);
+    let idade = campoIdade.value();
+    let gostaDeFantasia = campoFantasia.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+    text(recomendacao, width / 2, height / 2);
+}
+function geraRecomendacao(idade, gostaDeFantasia) {
+    if(idade >= 10) {
+        if(idade >= 14) {
+            return "O menino que descobriu o vento";
+        } else {
+            if(gostaDeFantasia){
+                return "As aventuras de Pi";
+            } else {
+                return "Depois da chuva";
+            }
+        }
+    } else {
+        if(gostaDeFantasia) {
+            return "A viagem de Chihiro";
+        } else {
+            return "O feitiço do tempo";
+        }
+    }
+}
